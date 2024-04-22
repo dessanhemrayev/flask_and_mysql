9b0ed41a7f1eda290b97d2c6b07b2263d54dea43
dessanhemrayev <43703214+dessanhemrayev@users.noreply.github.com>
2024-04-22 08:25:31 +0000

    chore: Refactoring code

--- app.py
+++ app.py
@@ -1,140 +1,118 @@
-from flask import Flask, render_template, request, redirect, url_for, session,Response
+from flask import Flask, render_template, request, redirect, url_for, session, Response
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import sqlite3
 
-from export_excel import excel_file
+from export_excel import generate_excel_file
 
 app = Flask(__name__)
- 
- 
-app.secret_key = 'your secret key'
- 
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'to-do'
- 
- 
+
+
+app.secret_key = "your secret key"
+
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "to-do"
+
+
 mysql = MySQL(app)
- 
- 
-                            # @app.route('/')
-@app.route('/login', methods =['POST'])
+
+
+@app.route("/login", methods=["POST"])
 def login():
-    msg = ''
-    print(request.form)
-    if request.method == 'POST' and 'fname' in request.form and 'lname' in request.form:
-        if request.form.get('action1'):                                                # 1 кнопка
-            fname = request.form['fname']
-            lname = request.form['lname']
+    msg = ""
+    if request.method == "POST" and "fname" in request.form and "lname" in request.form:
+        if request.form.get("action1"):  # 1 кнопка
+            fname = request.form["fname"]
+            lname = request.form["lname"]
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE login = % s ', (fname,))
-            cursor.execute('SELECT * FROM user')
-        # result = cursor.fetchone() # fetchone() выводит одну строчку
-            result = cursor.fetchall() #   в
-            print(result)
+            cursor.execute("SELECT * FROM user")
+            result = cursor.fetchall()  #   в
             if result:
-                keys=list(result[0].keys())
-                print(keys)
-            # session['loggedin'] = True
-            # session['id'] = result['id']
-            # session['fname'] = result['login']
-                msg = 'Logged in successfully !'
-                data={'user_name':fname,'project_name':lname}
-                return render_template('qwe.html', msg = result,keys=keys,data=data)
+                keys = list(result[0].keys())
+                msg = "Logged in successfully !"
+                data = {"user_name": fname, "project_name": lname}
+                return render_template("project.html", msg=result, keys=keys, data=data)
 
-
-        if request.form.get('action2'):                                             #  2 кнопка
-            fname = request.form['fname']
-            lname = request.form['lname']
+        if request.form.get("action2"):  #  2 кнопка
+            fname = request.form["fname"]
+            lname = request.form["lname"]
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE login = % s ', (fname,))
-            cursor.execute('SELECT id, login FROM user WHERE login = % s ', (fname,))
-            result = cursor.fetchall() 
-            print(result)
+            cursor.execute("SELECT id, login FROM user WHERE login = % s ", (fname,))
+            result = cursor.fetchall()
             if result:
-                keys=list(result[0].keys())
-                data={'user_name':fname,'project_name':lname}
-                return render_template('qwe.html', msg = result,keys=keys,data=data)
+                keys = list(result[0].keys())
+                data = {"user_name": fname, "project_name": lname}
+                return render_template("project.html", msg=result, keys=keys, data=data)
 
-    
-                
-        if request.form.get('action3'):                                             #  3 кнопка
-            fname = request.form['fname']
-            lname = request.form['lname']
+        if request.form.get("action3"):  #  3 кнопка
+            fname = request.form["fname"]
+            lname = request.form["lname"]
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE login = % s ', (fname,))
-            cursor.execute('DELETE FROM user WHERE login = % s ', (fname,))   
-            print('удаление выполнено')
-            mysql.connection.commit() 
-            cursor.close()
-            # if result:
-            #     keys=list(result[0].keys())
-            #     print(keys)
-            #     msg = 'Logged in successfully !'
-            #     return render_template('qwe.html', msg = result,keys=keys)
-        
-        if request.form.get('action4'):                                             #  4 кнопка
-            fname = request.form['fname']
-            lname = request.form['lname']
-            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        # cursor.execute('SELECT * FROM user WHERE login = % s ', (fname,))
-            cursor.execute('INSERT INTO user (login, email,parol)  VALUES ("коля", "апв","1235");')
-         
-            print('запись добавлена')
+            cursor.execute("DELETE FROM user WHERE login = % s ", (fname,))
             mysql.connection.commit()
             cursor.close()
-    
+
+        if request.form.get("action4"):  #  4 кнопка
+            fname = request.form["fname"]
+            lname = request.form["lname"]
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute(
+                'INSERT INTO user (login, email,parol)  VALUES ("коля", "апв","1235");'
+            )
+
+            mysql.connection.commit()
+            cursor.close()
+
     else:
-        msg = 'Incorrect fname / lname !'
-    return render_template('qwe.html', msg = msg)
+        msg = "Incorrect fname / lname !"
+    return render_template("project.html", msg=msg)
 
 
-@app.route('/export', methods =['POST'])
+@app.route("/export", methods=["POST"])
 def export():
+    if request.form.get("action5"):
+        fname = request.form["fname"]
+        lname = request.form["lname"]  #  5 кнопка
+        control = "*"
+        if lname != "type" or fname != "type":
+            control = "id,login"
 
-    if request.form.get('action5'): 
-        fname = request.form['fname']
-        lname = request.form['lname']                                            #  5 кнопка
-        control='*'
-        if lname!='type' or fname!='type':
-            control='id,login'
-
-        query = f'SELECT {control} FROM user'
-        fields = ['fname','lname']
-        hash_fields = {'fname':'login','lname':'id'}
+        query = f"SELECT {control} FROM user"
+        fields = ["fname", "lname"]
+        hash_fields = {"fname": "login", "lname": "id"}
         add_query = []
         add_query_value = []
         for item in fields:
-            if request.form[item]!='type':
+            if request.form[item] != "type":
                 add_query.append(f"{hash_fields[item]} = % s")
-                add_query_value.append(request.form[item])  
+                add_query_value.append(request.form[item])
         if len(add_query):
-            query += ' WHERE '+' or '.join(add_query)
-        print(query)
-        print(add_query_value)
+            query += " WHERE " + " or ".join(add_query)
+
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # cursor.execute('SELECT * FROM user WHERE login = % s ', (fname,))
         cursor.execute(query, tuple(add_query_value))
-        result = cursor.fetchall() 
-        print(result)
+        result = cursor.fetchall()
         if result:
-            keys=list(result[0].keys())
-            output = excel_file(keys,result)  #формирование файла для скачивания 
-            print(output) 
-            filename = 'otchet'
-            
-            return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition":f"attachment;filename={filename}.xls"},)
+            keys = list(result[0].keys())
+            output = generate_excel_file(
+                keys, result
+            )  # формирование файла для скачивания
+            filename = "otchet"
+            return Response(
+                output,
+                mimetype="application/ms-excel",
+                headers={"Content-Disposition": f"attachment;filename={filename}.xls"},
+            )
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port="8888")
-
-    
--- export_excel.py
+++ export_excel.py
@@ -1,31 +1,28 @@
 import io
 import xlwt
 
-def excel_file(headers,data):
-    print("функция вошли")
-    print(headers,data)
-     #output in bytes
-    output = io.BytesIO()
-    #create WorkBook object
-    workbook = xlwt.Workbook()
-    #add a sheet
-    sh = workbook.add_sheet('Employee Report')
 
-    #add headers
+def generate_excel_file(headers, data):
+    """Generated Excel file by data"""
+    # output in bytes
+    output = io.BytesIO()
+    # create WorkBook object
+    workbook = xlwt.Workbook()
+    # add a sheet
+    sh = workbook.add_sheet("Employee Report")
+
+    # add headers
     head = 0
     for header in headers:
         sh.write(0, head, header)
         head += 1
     idx = 0
-    #     keys=list(result[0].keys())
     for row in data:
         head = 0
         for header in headers:
-            sh.write(idx+1, head, row[header])
+            sh.write(idx + 1, head, row[header])
             head += 1
         idx += 1
-
     workbook.save(output)
-    print("функция выщли")
     output.seek(0)
     return output
